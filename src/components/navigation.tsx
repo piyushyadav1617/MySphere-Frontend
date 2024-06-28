ff090a87d60c65bdcdcb8c0c36955025faaab700
piyush_yadav <yadavpiyush222@gmail.com>
2024-06-28 20:29:22 +0530

    initial commit

---
+++ src/components/navigation.tsx
@@ -0,0 +1,242 @@
+import { Link, Outlet, NavLink } from "react-router-dom"
+import { Home, Search, MessageCircleMore, Heart, CircleUser, Settings, Camera } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react"
+// import { SearchComponent } from "./search"
+import { Input } from "@/components/ui/input"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
+const Navigation = () => {
+    const [expanded, setExpanded] = useState(true);
+    const [searchExpanded, setSearchExpanded] = useState(false);
+    const [notifExpanded, setNotifiExpanded] = useState(false);
+    useEffect(()=>{
+      if(expanded){
+        setSearchExpanded(false);
+        setNotifiExpanded(false);
+      }else if(notifExpanded){
+        setSearchExpanded(false);
+        setExpanded(false);
+      }else if(searchExpanded){
+        setExpanded(false);
+        setNotifiExpanded(false);
+      }
+
+    },[expanded, searchExpanded, notifExpanded])
+    return (
+        <div className="lg:flex lg:flex-row">
+            <header className="lg:hidden sticky top-0 w-full z-10 h-14 flex items-center justify-between px-4 border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/80">
+
+                <h1 className="text-2xl font-semibold tracking-tight"><Link to="/">MySphere</Link></h1>
+                <div className="flex gap-2">
+                    <Button variant={"ghost"} size={"icon"}>
+                        <Search
+                            className={`h-6 w-6`}
+                        />
+                    </Button>
+                    <Button variant={"ghost"} size={"icon"}>
+                        <Heart
+                            className={`h-6 w-6`}
+                        />
+                    </Button>
+                </div>
+            </header>
+            <aside className={`hidden lg:block z-20 flex-shrink-0 h-screen ${expanded ? "border-r" : ""} px-4 py-8 sticky top-0 left-0 bg-background`}>
+                <div className="mb-8 p-2">
+                    <Link to="/">
+                        <h1 className={`${expanded ? "block" : "hidden"} mx-auto text-2xl font-semibold tracking-tight`}>MySphere</h1>
+                        <Camera className={`${expanded ? "hidden" : " block opacity-100 h-8 w-8"} mx-auto `} />
+                    </Link>
+                </div>
+                <nav>
+                    <ul className="space-y-2">
+                        <li>
+                            <Button asChild variant={"ghost"} className={`${expanded ? "w-full" : "w-fit"} group flex justify-start  text-lg font-normal rounded-lg py-6`}
+                                onClick={() => (setExpanded(true))}
+                            >
+                                <NavLink to="/">
+                                    <Home
+                                        className={`h-6 w-6 group-hover:scale-110 transition-all`}
+                                    />
+                                    <span className={`${expanded ? "w-fit opacity-100 ml-6" : "w-0  opacity-0 overflow-hidden"} transition-all`}> Home</span>
+
+
+                                </NavLink>
+                            </Button>
+                        </li>
+                        <li>
+
+                            <Button variant={"ghost"} className={`${expanded ? "w-full" : "w-fit"} group flex justify-start  text-lg font-normal rounded-lg py-6`}
+                                onClick={() => {
+                                    if(expanded){
+                                       setExpanded(false);
+                                       setSearchExpanded(true);
+                                    }else if(notifExpanded){
+                                        setExpanded(false);
+                                        setNotifiExpanded(false);
+                                        setSearchExpanded(true);
+                                    }if(searchExpanded){
+                                        setSearchExpanded(false);
+                                        setNotifiExpanded(false)
+                                        setExpanded(true);
+                                    }
+                                }}>
+                                <Search
+                                    className={`h-6 w-6 group-hover:scale-110 transition-all`}
+                                />
+                                <span className={`${expanded ? "w-fit opacity-100 ml-6" : "w-0  opacity-0 overflow-hidden"} transition-all`}>Search</span>
+
+                            </Button>
+
+                        </li>
+                        <li>
+                            <Button asChild variant={"ghost"} className={`${expanded ? "w-full" : "w-fit"} group flex justify-start  text-lg font-normal rounded-lg py-6`}
+                                onClick={() => (setExpanded(true))}
+                            >
+                                <NavLink to="/messages">
+                                    <MessageCircleMore
+                                        className={`h-6 w-6 group-hover:scale-110 transition-all`}
+                                    />
+                                    <span className={`${expanded ? "w-fit opacity-100 ml-6" : "w-0 opacity-0 overflow-hidden"} transition-all`}>Messages</span>
+                                </NavLink>
+                            </Button>
+                        </li>
+                        <li>
+                            <Button variant={"ghost"} className={`${expanded ? "w-full" : "w-fit"} group flex justify-start  text-lg font-normal rounded-lg py-6`}
+                                 onClick={() => {
+                                    if(expanded){
+                                       setExpanded(false);
+                                       setNotifiExpanded(true);
+                                    }else if(searchExpanded){
+                                        setExpanded(false);
+                                        setSearchExpanded(false);
+                                        setNotifiExpanded(true);
+                                    }if(notifExpanded){
+                                        setNotifiExpanded(false);
+                                        setSearchExpanded(false)
+                                        setExpanded(true);
+                                    }
+                                }}
+                            >
+                                <Heart
+                                    className={`h-6 w-6 group-hover:scale-110 transition-all`}
+                                />
+                                <span className={`${expanded ? "w-fit opacity-100 ml-6" : "w-0  opacity-0 overflow-hidden"} transition-all`}>Notifications</span>
+
+
+                            </Button>
+                        </li>
+                        <li>
+                            <Button asChild variant={"ghost"} className={`${expanded ? "w-full" : "w-fit"} group flex justify-start  text-lg font-normal rounded-lg py-6`}
+                                onClick={() => (setExpanded(true))}
+                            >
+                                <NavLink to="/sabrinacarpenter">
+                                    <CircleUser
+                                        className={`h-6 w-6 group-hover:scale-110 transition-all`}
+                                    />
+                                    <span className={`${expanded ? "w-fit opacity-100 ml-6" : "w-0  opacity-0 overflow-hidden"} transition-all`}> Profile</span>
+
+
+                                </NavLink>
+                            </Button>
+                        </li>
+                        <li>
+                            <Button variant={"ghost"} className={`${expanded ? "w-full" : "w-fit"} group flex justify-start  text-lg font-normal rounded-lg py-6`}>
+                                <Settings
+                                    className={`h-6 w-6 group-hover:scale-110 transition-all`}
+                                />
+                                <span className={`${expanded ? "w-fit opacity-100 ml-6" : "w-0  opacity-0 overflow-hidden"} transition-all`}>More</span>
+
+                            </Button>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+
+            {/* the search component */}
+            <div className={`${searchExpanded && !expanded ? "translate-x-0 " : "-translate-x-[22rem] hidden"} w-80 transition-all px-4 py-8 hidden lg:block z-10 flex-shrink-0 h-screen fixed top-0 left-[72px] border-r bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/80 `}>
+                <div className="border-b">
+                    <h1 className={`w-fit text-2xl`}>Search</h1>
+
+                    <Input type="text" placeholder="Search" className={`p-x-4 my-8  outline-none ring-0 focus-visible:ring-0 focus-visible:ring-offset-0 bg-primary-foreground`} />
+                </div>
+                <div className="overflow-y-auto py-4">
+
+                </div>
+
+            </div>
+
+            {/* the Notification component */}
+            <div className={`${notifExpanded && !searchExpanded && !expanded ? "translate-x-0 " : "-translate-x-[22rem] hidden"} w-80 transition-all px-4 py-8 hidden lg:block z-10 flex-shrink-0 h-screen fixed top-0 left-[72px] border-r bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/80 `}>
+                <div className="">
+                    <h1 className={`w-fit text-2xl`}>Notifications</h1>
+                </div>
+                <div className="overflow-y-auto py-4">
+                    <div className="h-fit p-2 rounded-lg hover:bg-primary-foreground flex flex-row tex-sm cursor-pointer">
+                        <div className="flex gap-2">
+                            <Avatar className="border h-10 w-10">
+                                <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH4PnzC50nS6p-Amu_gSiNpAoulvk0YM0Zug&s" />
+                                <AvatarFallback>AC</AvatarFallback>
+                            </Avatar>
+                            <p>
+                                A random notification with random emojis 🎂❤️
+                            </p>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+
+            <Outlet />
+
+            <footer className="lg:hidden border-t fixed bottom-0 bg-background w-full px-4">
+                <nav>
+                    <ul className="flex items-center justify-around h-16 p-0">
+                        <li>
+                            <Button asChild variant={"ghost"} size={"icon"}>
+                                <NavLink to="/">
+                                    <Home
+                                        className={`h-6 w-6`}
+                                    />
+                                </NavLink>
+                            </Button>
+                        </li>
+                        <li>
+                            <Button asChild variant={"ghost"} size={"icon"}>
+                                <NavLink to="/messages">
+                                    <MessageCircleMore
+                                        className={`h-6 w-6`}
+                                    />
+                                </NavLink>
+                            </Button>
+                        </li>
+                        <li>
+                            <Button asChild variant={"ghost"} size={"icon"}>
+                                <NavLink to="/sabrinacarpenter">
+                                    <CircleUser
+                                        className={`h-6 w-6`}
+                                    />
+                                </NavLink>
+                            </Button>
+                        </li>
+                        <li>
+                            <Button variant={"ghost"} size={"icon"}>
+                                <Settings
+                                    className={`h-6 w-6`}
+                                />
+                            </Button>
+                        </li>
+
+                    </ul>
+                </nav>
+
+
+            </footer>
+        </div>
+    )
+}
+
+export default Navigation
